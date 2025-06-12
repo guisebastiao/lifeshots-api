ca1e0e863529fca1b0a5cdc1553320ec25473f1a
GuiSebastiao <guilhermesebastiaou.u@gmail.com>
2025-06-12 13:47:04 -0300

    arquivos iniciais

---
+++ src/test/java/com/lifeshots/lifeshotsapi/LifeshotsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifeshots.lifeshotsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeshotsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
